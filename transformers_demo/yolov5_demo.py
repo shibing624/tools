0b4ccdfb10c66078322b2e1bcb292caa63db284f
flemingxu <flemingxu@tencent.com>
2022-11-28 17:39:36 +0800

    update yolov5

---
+++ transformers_demo/yolov5_demo.py
@@ -0,0 +1,15 @@
+import torch
+torch.hub._validate_not_a_forked_repo=lambda a,b,c: True
+# 模型
+model = torch.hub.load('ultralytics/yolov5', 'yolov5s')  # or yolov5n - yolov5x6, custom
+
+# 图像
+img = 'zidane.jpg'  # or file, Path, PIL, OpenCV, numpy, list
+
+# 推理
+results = model(img)
+
+# 结果
+print(results.print())  # or .show(), .save(), .crop(), .pandas(), etc.
+results.save()
+
