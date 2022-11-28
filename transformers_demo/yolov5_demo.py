66a5342835cfea6218801e0fde4eb5841e47fc8c
shibing624 <shibing624@126.com>
2022-11-28 18:32:50 +0800

    update yolo.

--- transformers_demo/yolov5_demo.py
+++ transformers_demo/yolov5_demo.py
@@ -1,5 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+@author:XuMing(xuming624@qq.com)
+@description:
+"""
 import torch
-torch.hub._validate_not_a_forked_repo=lambda a,b,c: True
+
+torch.hub._validate_not_a_forked_repo = lambda a, b, c: True
 # 模型
 model = torch.hub.load('ultralytics/yolov5', 'yolov5s')  # or yolov5n - yolov5x6, custom
 
@@ -11,5 +17,9 @@ results = model(img)
 
 # 结果
 print(results.print())  # or .show(), .save(), .crop(), .pandas(), etc.
-results.save()
+results.save(save_dir="zidane_res.jpg")
 
+print(results.pandas().xyxy[0])
+
+json_r = results.pandas().xyxy[0].to_json(orient="records")  # JSON img1 predictions
+print(json_r, type(json_r))
